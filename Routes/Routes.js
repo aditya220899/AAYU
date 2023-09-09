bb59f9cd5040996b48190c929ed0b5c1decff955
Aditya Kumar Pandey <120619731+aditya220899@users.noreply.github.com>
2023-09-09 14:20:55 +0530

    Update Routes.js

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const router =express.Router()
+const MyControllers = require('../Controllers/Controllers')
 
+router.get('/', MyControllers.getApi)
+router.post('/add-user' , MyControllers.RegisterUser)
+router.post('/login-user' , MyControllers.loginUser)
+router.post('/add-product' , MyControllers.addProduct)
+router.get('/get-products', MyControllers.getAllProducts)
+router.post('/add-to-cart', MyControllers.addToCart)
+router.get('/get-cart-count' , MyControllers.getCartCountByUserID)
+router.get('/get-cart-with-products' , MyControllers.getCartProducts)
+router.get('/get-cart-with-products-2' , MyControllers.getCartProducts2)
+router.post('/update-cart-quantity' , MyControllers.updateCartQuantity)
+router.post('/update-addresses' , MyControllers.addAddress)
+router.get('/remove-cart-item' , MyControllers.RemoveItemFromCart)
+router.get('/get-user-addresses' , MyControllers.getUserAddresses)
+router.post('/place-order' , MyControllers.addOrder)
+
+module.exports = router
